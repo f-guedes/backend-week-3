444b16486af300c3036d0930007f5a74b3f9552e
f-guedes <fernando.hpg2@gmail.com>
2022-09-20 16:36:31 -0400

    exercises: methods

---
+++ src/exercises/Methods.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		// Methods
+		
+		String firstName = "Gabriel";
+		String lastName = "Pereira";
+		String fullName = createFullName(firstName, lastName);
+		String fullName2 = createFullName("Eduardo", lastName);
+		
+		System.out.println(fullName);
+		System.out.println(fullName2);
+
+	}
+
+	public static String createFullName(String x, String z) {
+		return x + " " + z;
+	}
+	
+}
